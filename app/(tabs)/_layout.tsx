fd6bfd7c8646d4a6e2927d6d33e9801b973a34b1
valdesik <valdesik000@gmail.com>
2025-03-13 21:48:54 +0100

    add new components. Also add few images.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,53 @@
-import {View, Text} from 'react-native'
+import {View, Text, ImageBackground, Image} from 'react-native'
 import React from 'react'
 import {Tabs} from "expo-router";
+import {images} from "@/constants/images";
+
+const TabIcon = (focuced, icon, title): any => {
+    if(focuced){
+    return (
+        <ImageBackground
+            source={images.highlight}
+            className='flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden'
+            style={{width: 25, height: 25}}>
+            <Image source={icon}
+                   tintColor='#151312'
+                   style={{
+                       width: 25,
+                       height: 25
+                   }}
+                   className='size-5'
+            />
+            <Text
+                className='text-secondary text-base font-semibold ml-2'
+            >{title}</Text>
+        </ImageBackground>
+    )}
+    else {
+        return (
+            <View className='size-full justify-center items-center mt-4 rounded-full'>
+            <Image source={icon}
+                   tintColor='#151312'
+                   className='size-5'
+            />
+            </View>
+        )
+    }
+}
 
 const _Layout = () => {
     return (
         <Tabs>
             <Tabs.Screen name="index" options={
-                {
-                    headerShown: false,
-                    title: 'Home'
-                }
+                {headerShown: false, title: 'Home', tabBarIcon: ({focused}) => {
+                        return (
+                            <>
+                                <TabIcon focused={focused}
+                                         icon={images.home}
+                                         title='Home'/>
+                            </>
+                        )
+                    }}
                 }
             />
             <Tabs.Screen name="saved"
